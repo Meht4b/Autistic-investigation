1000e4db8f06903e8e26136ab4efa983d6622f88
Trimpta <68428753+trimpta@users.noreply.github.com>
2023-11-09 20:29:41 +0530

    Update client.py

--- client.py
+++ client.py
@@ -17,7 +17,8 @@ try:
     data = pickle.load(f)                  #data=(username,password)
     username = pickle.load(f)[0]
     password = pickle.load(f)[1]
-    server.send(pickle.dump(username,password,""))
+    server.send(pickle.dump(username,password,"L"))
+    #attempts to read local file and login
 
 except FileNotFoundError:
     f = open('locahost','wb')
@@ -31,5 +32,7 @@ except FileNotFoundError:
     pickle.dump(l_details,f)
     server.send(pickle.dump(username,password,"S"))
     server.send(pickle.dump(l_details))
+    #Creates account and sends request
+
 
 
