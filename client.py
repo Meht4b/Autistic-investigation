81c413893afd773422febac56aa1918e16051f95
Trimpta <68428753+trimpta@users.noreply.github.com>
2023-11-09 23:31:51 +0530

    Update client.py

--- client.py
+++ client.py
@@ -7,23 +7,31 @@ password=None
 host ='localhost'
 port = 8080
 
-#Connect to server
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.connect((host,port))
-socket.setdefaulttimeout(18.5)
+def connect(host,port):
 
+    #Connect to server
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.connect((host,port))
+    socket.setdefaulttimeout(18.5)
 
-try:
+    return server
+
+def login():
 
     #Check if user has an account, if not create account and save in binary file
     try:
         f           = open('localdat','rb')
-        data        = pickle.load(f)                  #data=(username,password)
+
         username    = pickle.load(f)[0]
         password    = pickle.load(f)[1]
 
         server.send(pickle.dump(username,password,"L"))
         response    = pickle.load(server.recv(6940))
+
+        if response[0]==True:
+            print(response[1])
+        elif response[0]==False:
+            print(response[1])
 
 
         
@@ -37,15 +45,25 @@ try:
         name        = input("Enter your name:")
         number      = input("Enter your number:")
         l_details   = (username,password,name,number)
-        server.recv()
 
         pickle.dump(l_details,f)
         server.send(pickle.dump(username,password,"S"))
         server.send(pickle.dump(l_details))
+        response    = pickle.load(server.recv(6940))
+
+
+
         #Creates account and sends request
 
-    while True:
-        pass        
-except Exception as doramon:
-    print(doramon)
+def transact(reciever,amount):
+    server.send(pickle.dump("transact",(to,amount)))
 
+def withdraw()
+        
+
+
+
+(withdraw,(amount))
+(deposit,(amount))
+(history,())
+(loan,(amount))
