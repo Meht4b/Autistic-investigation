5536ddeeea4465d1eafba4ae298609723b6e1f93
Mehthab <114091862+Meht4b@users.noreply.github.com>
2023-11-09 19:13:53 +0530

    thing

---
+++ database.py
@@ -0,0 +1,10 @@
+#abhishek
+
+def init():
+    pass
+
+def user_check():
+    pass
+
+def acc_id():
+    pass
--- server.py
+++ server.py
@@ -15,19 +15,26 @@ server.bind((host,port))
 server.listen()
 
 
+
 def handleClient(conn):
 
 
     try:
         #checks username and password;closes if they do not match
         username,password = pickle.loads(conn.recv(100))
+
         if database.user_check(username,password):
             conn.send(pickle.dump(True))
+            acc_id = database.acc_id(username,password)
+
         else:
             conn.send(pickle.dump(False))
             conn.close()
             return None
         
+        #request cycle 
+
+
     
     except:
         conn.send(pickle.dump(False))
