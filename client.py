47f41f49e5f41d0e571ef728e678c1b55ac02786
Trimpta <68428753+trimpta@users.noreply.github.com>
2023-11-09 20:50:32 +0530

    Update client.py

--- client.py
+++ client.py
@@ -10,29 +10,42 @@ port = 8080
 #Connect to server
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.connect((host,port))
+socket.setdefaulttimeout(18.5)
 
-#Check if user has an account, if not create account and save in binary file
+
 try:
-    f = open('localdat','rb')
-    data = pickle.load(f)                  #data=(username,password)
-    username = pickle.load(f)[0]
-    password = pickle.load(f)[1]
-    server.send(pickle.dump(username,password,"L"))
-    #attempts to read local file and login
 
-except FileNotFoundError:
-    f = open('locahost','wb')
-    print("You are creating a new account")
-    username    = input("Enter username:")
-    password    = input("Enter password:")
-    name        = input("Enter your name:")
-    number      = input("Enter your number:")
-    l_details   = (username,password,name,number)
+    #Check if user has an account, if not create account and save in binary file
+    try:
+        f           = open('localdat','rb')
+        data        = pickle.load(f)                  #data=(username,password)
+        username    = pickle.load(f)[0]
+        password    = pickle.load(f)[1]
 
-    pickle.dump(l_details,f)
-    server.send(pickle.dump(username,password,"S"))
-    server.send(pickle.dump(l_details))
-    #Creates account and sends request
+        server.send(pickle.dump(username,password,"L"))
+        response    = pickle.load(server.recv(6940))
 
 
+        
+        #attempts to read local file and login
+
+    except FileNotFoundError:
+        f = open('locahost','wb')
+        print("You are creating a new account")
+        username    = input("Enter username:")
+        password    = input("Enter password:")
+        name        = input("Enter your name:")
+        number      = input("Enter your number:")
+        l_details   = (username,password,name,number)
+        server.recv()
+
+        pickle.dump(l_details,f)
+        server.send(pickle.dump(username,password,"S"))
+        server.send(pickle.dump(l_details))
+        #Creates account and sends request
+
+    while True:
+        pass        
+except Exception as doramon:
+    print(doramon)
 
