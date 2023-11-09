661827d04108240128d633dc8a855fcc1a72f4ab
Mehthab <114091862+Meht4b@users.noreply.github.com>
2023-11-09 20:11:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -60,6 +60,7 @@ def handleClient(conn):
 
             #history 
             if req[0]=='history':
+                conn.send(pickle.dump(database.history(acc_id)))
                 
 
                 
