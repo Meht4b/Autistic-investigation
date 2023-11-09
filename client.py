3e98e8640e6a9cd0d3761ea0786ec24c67f1bc8d
Trimpta <68428753+trimpta@users.noreply.github.com>
2023-11-09 20:10:26 +0530

    Update client.py

--- client.py
+++ client.py
@@ -4,16 +4,31 @@ import pickle
 username=None
 password=None
 
-#Check if user has an account, if not create account and save in binary file
+host ='localhost'
+port = 8080
 
+#Connect to server
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.connect((host,port))
+
+#Check if user has an account, if not create account and save in binary file
 try:
     f = open('localdat','rb')
     data = pickle.load(f)                  #data=(username,password)
-    username,password=pickle.load(f)[0],pickle.load(f)[1]
-
+    username = pickle.load(f)[0]
+    password = pickle.load(f)[1]
+    server.send(pickle.dump(username,password,""))
 except FileNotFoundError:
-    f=open('locahost','wb')
-        
+    f = open('locahost','wb')
+    print("You are creating a new account")
+    username    = input("Enter username:")
+    password    = input("Enter password:")
+    name        = input("Enter your name:")
+    number      = input("Enter your number:")
+    l_details   =(username,password,name,number)
 
-#Login
-def login():
+    pickle.dump(l_details,f)
+    server.send(pickle.dump(username,password,"S"))
+    server.send(pickle.dump(l_details))
+
+
